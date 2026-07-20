4b1be09e998186027cf20cd784bac909d525a3f5
Mohiuddin <mohiuddinn714@gmail.com>
2026-07-21 00:51:51 +0600

    added Module-11

--- Module-11/index.js
+++ Module-11/index.js
@@ -1,6 +1,85 @@
 // Conceptual Session for Javascript variables, conditionals(if-else, if-else if - else), loop(for, while, do-while) and array
 
+/*
+ * Task 1
+ */
+const numbers = [18, 5, 27, 3, 14, 9];
+let min = numbers[0];
 
-/* 
-* Task 1
-*/
+for (const number of numbers) {
+  if (number < min) {
+    min = number;
+  }
+}
+// console.log('Minimum number is ', min); // 3
+
+/*
+ * Task 2
+ */
+const numbers2 = [12, 7, 12, 5, 7, 20, 12];
+let count = 0;
+
+for (let num of numbers2) {
+  if (num === 12) {
+    count++;
+  }
+}
+// console.log(count) // 3
+
+/*
+ * Task 3
+ */
+const numbers3 = [5, 8, 12, 3, 18, 7, 20];
+const even = [];
+const odd = [];
+
+for (const num of numbers3) {
+  if (num % 2 === 0) {
+    even.push(num);
+  } else {
+    odd.push(num);
+  }
+}
+// console.log(even , odd); //[ 8, 12, 18, 20 ] [ 5, 3, 7 ]
+
+/*
+ * Task 4
+ */
+const numbers4 = [10, 20, 30, 40, 50];
+const reverseArray = [];
+
+for (const num of numbers4) {
+  reverseArray.unshift(num);
+}
+// console.log(reverseArray); //[ 50, 40, 30, 20, 10 ]
+
+/*
+ * Task 5
+ */
+const numbers5 = [5, 12, 7, 18, 20, 3, 11];
+let sumOfEven = 0;
+
+for (const num of numbers5) {
+  if (num % 2 === 0) {
+    sumOfEven += num;
+  }
+}
+// console.log(sumOfEven); //50
+
+/*
+ * Task 6 || very hard
+ */
+const numbers6 = [5, 8, 5, 12, 8, 20, 3];
+const arrayOfDuplicates = [];
+const seen = [];
+
+for (const num of numbers6) {
+  if (seen.includes(num)) {
+   if(!arrayOfDuplicates.includes(num)){
+      arrayOfDuplicates.push(num)
+   }
+  } else {
+   seen.push(num)
+  }
+}
+console.log(arrayOfDuplicates);
